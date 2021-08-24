1ea4e75e52569dfec1610ab81eed004b3219659d
Fabio T Alves <ftalves@poli.ufrj.br>
2021-08-24 16:50:29 -0300

    Using loop to render graphs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <vue-plotly :data="data1" :layout="layout1" :options="options" />
-    <vue-plotly :data="data2" :layout="layout2" :options="options" />
+    <div v-for="(item, index) in object.graphs" :key="index">
+      <vue-plotly
+        :data="item.data"
+        :layout="item.layout"
+        :options="object.options"
+      />
+    </div>
   </div>
 </template>
 
@@ -15,70 +20,80 @@ export default {
   },
   data() {
     return {
-      data1: [
-        {
-          values: [20700, 21800, 2300, 25200, 20100, 20800, 19600],
-          labels: [
-            'agent',
-            'claims_ins',
-            'claim_own',
-            'claim_pending',
-            'debt_drp',
-            'debt_drp_tax',
-            'debt_vet',
-          ],
-          domain: { column: 0 },
-          name: '',
-          hoverinfo: 'label+percent+value',
-          hole: 0.5,
-          type: 'pie',
-        },
-      ],
-      data2: [
-        {
-          x: [1, 2, 3, 4],
-          y: [10, 15, 13, 17],
-          type: 'scatter',
-        },
-      ],
-      layout1: {
-        title: '',
-        annotations: [
+      object: {
+        graphs: [
           {
-            font: {
-              size: 32,
+            data: [{
+              values: [20700, 21800, 2300, 25200, 20100, 20800, 19600],
+              labels: [
+                'agent',
+                'claims_ins',
+                'claim_own',
+                'claim_pending',
+                'debt_drp',
+                'debt_drp_tax',
+                'debt_vet',
+              ],
+              domain: { column: 0 },
+              name: '',
+              hoverinfo: 'label+percent+value',
+              hole: 0.5,
+              type: 'pie',
+            }],
+            layout: {
+              title: '',
+              annotations: [
+                {
+                  font: {
+                    size: 32,
+                  },
+                  showarrow: false,
+                  text: 'Balans',
+                  x: 0.5,
+                  y: 0.5,
+                },
+              ],
+              height: 600,
+              width: 800,
+              showlegend: true,
             },
-            showarrow: false,
-            text: 'Balans',
-            x: 0.5,
-            y: 0.5,
+          },
+          {
+            data: [{
+              x: [1, 2, 3, 4],
+              y: [10, 15, 13, 17],
+              type: 'scatter',
+            }],
+            layout: {
+              title: '',
+              annotations: [
+                {
+                  font: {
+                    size: 20,
+                  },
+                  showarrow: false,
+                  text: 'CO2',
+                  x: 0.82,
+                  y: 0.5,
+                },
+              ],
+              height: 600,
+              width: 800,
+              showlegend: true,
+            },
           },
         ],
-        height: 600,
-        width: 800,
-        showlegend: true,
+        options: { responsive: true },
       },
-      layout2: {
-        title: '',
-        annotations: [
-          {
-            font: {
-              size: 20,
-            },
-            showarrow: false,
-            text: 'CO2',
-            x: 0.82,
-            y: 0.5,
-          },
-        ],
-        height: 600,
-        width: 800,
-        showlegend: true,
-      },
-      options: { responsive: true },
     };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+div {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
